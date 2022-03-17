2a89fb24bf065dd14ca71892868f1b1296575d31
RandumbRobot <jacobyr2010@hotmail.ca>
2022-03-17 19:05:09 -0400

    Basics

---
+++ drive_control/src/drive_safety_system.py
@@ -0,0 +1,18 @@
+
+
+ROBOT = "CIRC"
+ROBOT = "ERC"
+
+if ROBOT is "CIRC":
+    MAX_SPEED = 10  # m/s
+    MAX_TIPPING_ANGLE_X  # degrees
+    MAX_TIPPING_ANGLE_Y  # degrees
+    MAX_TIPPING_ANGLE_Z  # degrees
+    MIN_DISTANCE = 2  # in meters - Minimum distance from an object to the depth camera (or Rover?)
+
+if ROBOT is "ERC":
+    MAX_SPEED = 10  # m/s
+    MAX_TIPPING_ANGLE_X  # degrees
+    MAX_TIPPING_ANGLE_Y  # degrees
+    MAX_TIPPING_ANGLE_Z  # degrees
+    MIN_DISTANCE = 1  # in meters - Minimum distance from an object to the depth camera (or Rover?)
